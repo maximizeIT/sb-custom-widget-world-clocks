d014c8960dde3f442833e8a1259c4b0dc4a78f8c
Max Scholz <max@staffbase.com>
2022-02-25 17:07:19 +0100

    fix errors

--- src/custom-widget-world-clocks.tsx
+++ src/custom-widget-world-clocks.tsx
@@ -2,7 +2,7 @@ import React, { ReactElement } from "react";
 import { BlockAttributes } from "widget-sdk";
 import CSS from "csstype";
 
-import '../resources/css/styles.css';
+// import '../resources/css/styles.css';
 
 // https://github.com/pvoznyuk/react-live-clock
 import Clock from 'react-live-clock';
@@ -18,47 +18,49 @@ export interface CustomWidgetWorldClocksProps extends BlockAttributes {
 export const CustomWidgetWorldClocks = ({ showtimeclock, showstandardlist, customclockactive, customclockformat, customclocktimezone, contentLanguage }: CustomWidgetWorldClocksProps): ReactElement => {
 
   const myClockStyles: CSS.Properties = {
-    textAlign: "center",
-    width: "15rem",
+    // textAlign: "center",
+    width: "100%",
     paddingTop: "20px",
+    fontSize: "50px",
+    color: "#7D00F9"
   };
 
   const isTimeClockShown = typeof showtimeclock == "string" ? showtimeclock === "true" : !!showtimeclock;
   const isStandardListShown = typeof showstandardlist == "string" ? showstandardlist === "true" : !!showstandardlist;
   const isCustomClockShown = typeof customclockactive == "string" ? customclockactive === "true" : !!customclockactive;
 
-  const updateTimeClock = () => {
+  // const updateTimeClock = () => {
     
-    const secondHand = document.querySelector('.second-hand');
-    const minsHand = document.querySelector('.min-hand');
-    const hourHand = document.querySelector('.hour-hand');
+  //   const secondHand = document.querySelector('.second-hand');
+  //   const minsHand = document.querySelector('.min-hand');
+  //   const hourHand = document.querySelector('.hour-hand');
 
-    function setDate() {
-      const now = new Date();
+  //   function setDate() {
+  //     const now = new Date();
 
-      const seconds = now.getSeconds();
-      const secondsDegrees = ((seconds / 60) * 360) + 90;
-      secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  //     const seconds = now.getSeconds();
+  //     const secondsDegrees = ((seconds / 60) * 360) + 90;
+  //     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
-      const mins = now.getMinutes();
-      const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
-      minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+  //     const mins = now.getMinutes();
+  //     const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+  //     minsHand.style.transform = `rotate(${minsDegrees}deg)`;
 
-      const hour = now.getHours();
-      const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
-      hourHand.style.transform = `rotate(${hourDegrees}deg)`;
-    }
+  //     const hour = now.getHours();
+  //     const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+  //     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+  //   }
 
-    setInterval(setDate, 1000);
+  //   setInterval(setDate, 1000);
 
-    setDate();
+  //   setDate();
 
-  }
+  // }
 
   return (<div>
     { isTimeClockShown ? 
         <div>
-          <div className="clock">
+          {/* <div className="clock">
             <div className="outer-clock-face">
               <div className="marking marking-one"></div>
               <div className="marking marking-two"></div>
@@ -70,9 +72,10 @@ export const CustomWidgetWorldClocks = ({ showtimeclock, showstandardlist, custo
                 <div className="hand second-hand"></div>
               </div>
             </div>
-        </div>
+        </div> */}
         <div style={myClockStyles}>
-          <Clock locale={contentLanguage} format={'HH:mm:ss a'} ticking={true} onChange={() => updateTimeClock()} />
+          {/* <Clock locale={contentLanguage} format={'HH:mm:ss a'} ticking={true} onChange={() => updateTimeClock()} /> */}
+          <Clock locale={contentLanguage} format={'HH:mm:ss a'} ticking={true} />
         </div>
         </div>
         : ''
